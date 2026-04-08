7734d5c13fb77408c6d46732c0401adfaf84eef2
Christopher Patti <feoh@feoh.org>
2026-04-08 04:36:22 -0400

    Add keybinding for undotree added in 0.12

--- lua/easeofuse.lua
+++ lua/easeofuse.lua
@@ -1,3 +1,6 @@
 -- Hit a prefix and show all potential keystrokes you can choose
 -- as well as what they do. Priceless!
 require("which-key").setup()
+
+-- Neovim 0.12 builtin undotree
+vim.keymap.set('n', '<leader>u', vim.cmd.Undotree, { desc = 'Toggle Undotree' })
